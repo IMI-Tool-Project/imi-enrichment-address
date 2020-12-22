1dd263106ec9011e978df069158c3692000636a2
indigo-lab <indigo-lab@users.noreply.github.com>
2020-12-22 16:11:34 +0900

    Fix 位置参照情報のダウンロード先が HTTP から HTTPS に変わったので変更

--- tools/download.sh
+++ tools/download.sh
@@ -12,7 +12,7 @@ mkdir -p ${CACHE_DIR}
 rm -f ${OUTPUT}
 
 for no in `seq -w 1000 1000 47000` ; do
-  url="http://nlftp.mlit.go.jp/isj/dls/data/12.0b/${no}-12.0b.zip"
+  url="https://nlftp.mlit.go.jp/isj/dls/data/12.0b/${no}-12.0b.zip"
   zip=${CACHE_DIR}/${no}.zip
   if [ ! -e "${zip}" ] ; then
     curl $url > ${zip}
